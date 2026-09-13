05a01f5fda15c73a46c907feea2e2d769f89299b
noskaia <103182008+jepiiii@users.noreply.github.com>
2026-09-14 00:08:22 +0800

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -2,6 +2,6 @@
 // Replace these two values after creating your Supabase project.
 // The anon key is designed to be used in browser apps when RLS is enabled.
 window.ECE_SUPABASE_CONFIG = Object.freeze({
-  SUPABASE_URL: 'https://YOUR-PROJECT.supabase.co',
-  SUPABASE_ANON_KEY: 'YOUR_SUPABASE_ANON_KEY'
+  SUPABASE_URL: 'https://wmjtcpghjbmljwhilzzy.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_-Gq1qL4HJhVXmhDfDo1Ysw_VfBRc7Do'
 });
